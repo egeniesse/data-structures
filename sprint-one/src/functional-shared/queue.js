65606ed5ac04e89a9a187b7ec1e9d907831c3925
egeniesse <egeniesse@gmail.com>
2015-11-18 15:59:35 -0800

    Created queue object, added methods to queue object, returned queue object

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,12 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.size = queueMethods.size;
-  this.enqueue = queueMethods.enqueue;
-  this.dequeue = queueMethods.dequeue;
-  this.stackSize = 0;
+  var queue = {};
+  queue.size = queueMethods.size;
+  queue.enqueue = queueMethods.enqueue;
+  queue.dequeue = queueMethods.dequeue;
+  queue.stackSize = 0;
 
-  return this;
+  return queue;
 };
 
 var queueMethods = {
