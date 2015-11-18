0cca32e1ab6e20e14ee8d4d5b5b330c295bb4fb0
egeniesse <egeniesse@gmail.com>
2015-11-18 14:45:05 -0800

    Returns 0 for new stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,22 +3,20 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size=0;
+ 
 
   // Implement the methods below
   someInstance.push = function(value) {
-    storage[size] = value;
-    size++;
+    
   };
 
   someInstance.pop = function() {
-    size--;
-    return storage[size];
+ 
 
   };
 
   someInstance.size = function() {
-    return size >= 0 ? size : 0;
+   return 0;
   };
 
   return someInstance;
