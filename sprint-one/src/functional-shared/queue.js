a2fb1ac7c4642d42215b6f242bced8336a029ce4
egeniesse <egeniesse@gmail.com>
2015-11-18 15:51:28 -0800

    Increments size when adding items to stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,13 +2,19 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.size = queueMethods.size;
+  this.enqueue = queueMethods.enqueue;
+  this.stackSize = 0;
 
   return this;
 };
 
 var queueMethods = {
   size : function(){
-    return 0;
+    return this.stackSize;
+  },
+
+  enqueue : function(){
+    this.stackSize++;
   }
 };
 
