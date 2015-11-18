5e715437688832cd4c22b77f93ba84005e525635
egeniesse <egeniesse@gmail.com>
2015-11-18 14:56:54 -0800

    Hard coded the size value to 0

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,26 +3,19 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = 0;
+ 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    storage[size]= value;
-    size++;
+    
   };
 
   someInstance.dequeue = function() {
-    var value = storage[0];
-    size--;
-    for(var i=0; i<size.length;i++){
-      storage[i] = storage[i+1];
-    }
-    console.log(storage)
-    return value;
+  
   };
 
   someInstance.size = function() {
-    return size >= 0 ? size : 0;
+    return 0;
   };
 
   return someInstance;
