98677f2b1f0e2e291b51ec168a2b1336c54f841b
egeniesse <egeniesse@gmail.com>
2015-11-19 14:10:43 -0800

    Allow users see if a tree or its children contains a target

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -17,7 +17,25 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
+  console.log(this);
+  var found = false;
 
+  if (this.value === target){
+      found = true;
+  }
+  var searchChildren = function(tree){
+    if (tree.children.length){
+      for (var i = 0; i < tree.children.length; i++){
+        if (tree.children[i].value === target){
+          found = true;
+        } else {
+          searchChildren(tree.children[i]);
+        } 
+      }
+    }
+  };
+  searchChildren(this);
+  return found;
 };
 
 
