4192568e1a94244b9a42559639821cc467ca5630
egeniesse <egeniesse@gmail.com>
2015-11-19 09:56:42 -0800

    Passes all tests for stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.container = {};
 };
 
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
 
+Stack.prototype.push = function(value){
+  this.container[this.stackSize]=value;
+  this.stackSize++;
+};
+
+Stack.prototype.pop = function(){
+  if(this.stackSize!==0){
+    this.stackSize--;
+    return this.container[this.stackSize];
+  };
+};
