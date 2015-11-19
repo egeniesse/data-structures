a0e1464f3c2c1580ffef584f3c241164dd609b15
egeniesse <egeniesse@gmail.com>
2015-11-19 15:28:54 -0800

    Stores value of new nodes

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,16 +3,22 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = node;
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if(this.nodes.hasOwnProperty(node)){
+    return true;
+  }
+  return false;
 };
 
 // ------------------------
