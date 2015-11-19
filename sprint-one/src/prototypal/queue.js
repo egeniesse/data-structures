da95df7824a799f95f113bd9ab012ad394188cc1
egeniesse <egeniesse@gmail.com>
2015-11-18 18:26:38 -0800

    added the stacksize variable as well as starting the enqueue function

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,15 +2,18 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var queue = Object.create(queueMethods);
-
+  queue.stackSize = 0;
 
   return queue;
 };
 
 var queueMethods = {
   size : function(){
-    return 0;
+    return this.stackSize;
   },
+  enqueue : function(){
+    this.stackSize++;
+  }
 };
 
 
