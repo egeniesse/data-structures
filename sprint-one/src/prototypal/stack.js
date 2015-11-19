042ba2144714a7f7dbe779e28cea3827603a8f47
egeniesse <egeniesse@gmail.com>
2015-11-18 18:19:02 -0800

    Updates stack size when adding an item

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -2,13 +2,16 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var stack = Object.create(stackMethods);
-
+  stack.stackSize =  0;
   return stack;
 };
 
 var stackMethods = {
   size : function(){
-    return 0;
+    return this.stackSize;
+  },
+  push: function(){
+    this.stackSize++;
   }
 };
 
