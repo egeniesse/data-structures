257ff4ebe19cf40171fa8fc15da420737437a464
egeniesse <egeniesse@gmail.com>
2015-11-18 16:24:34 -0800

    created a push method to go with the stack object

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,14 +2,22 @@ var Stack = function() {
   var stack = {};
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  stack.stackSize = 0;
+  stack.push = stackMethods.push;
   stack.size = stackMethods.size;
   return stack;
+
 };
 
 var stackMethods = {
   size : function(){
-    return 0;
-  }
+    return this.stackSize;
+
+  },
+  push : function(){
+    this.stackSize++;
+  },
 };
 
 
