641715e9f7350098fa5d3ae66fd8ed8d701a6dcb
egeniesse <egeniesse@gmail.com>
2015-11-19 10:01:36 -0800

    passes all of the tests for QUEUE!!!

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.up = 0;
+  this.container = {};
 };
 
+Queue.prototype.enqueue = function(value){
+  this.container[this.stackSize] = value;
+  this.stackSize ++;
+};
+
+Queue.prototype.size = function(){
+  return this.stackSize-this.up;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.stackSize-this.up>0){
+    var result = this.container[this.up];
+    this.up++;
+    return result;
+  }
+};
 
