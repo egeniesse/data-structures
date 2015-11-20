2c4788373162b0a57657155ed37a5a2f752b3e78
egeniesse <egeniesse@gmail.com>
2015-11-19 20:50:43 -0800

    Store values that were inserted

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage.get(index)=== undefined){
+    this._storage.set(index,v);
+  } else {
+    this._storage.each(function(value, key, library){
+      if (value === undefined){
+        this._storage.set(key, v);
+      }
+    });
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, key, library){
+    if(index === key){
+      delete library[key];
+    }
+  })
 };
 
 
