0693fa9d288e78631c7d66e90363575d05eed0d4
egeniesse <egeniesse@gmail.com>
2015-11-20 12:33:43 -0800

    Updated contains method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,8 +1,8 @@
 var BinarySearchTree = function(value) {
   var bsTree = Object.create(BinarySearchTree.prototype);
   bsTree.value = value;
-  bsTree.left = null;
-  bsTree.right = null;
+  bsTree.left = undefined;
+  bsTree.right = undefined;
   return bsTree;
   
 };
@@ -11,13 +11,13 @@ BinarySearchTree.prototype.insert = function(value){
   var node = new Node(value);
   var findEmptySpace = function(currentNode, nextNode){
     if(currentNode.value < nextNode.value){
-      if(currentNode.right === null){
+      if(currentNode.right === undefined){
         currentNode.right = nextNode;
       } else {
         findEmptySpace(currentNode.right, nextNode)
       }
     } else if(currentNode.value > nextNode.value){
-      if(currentNode.left === null){
+      if(currentNode.left === undefined){
         currentNode.left = nextNode;
       } else {
         findEmptySpace(currentNode.left, nextNode)
@@ -26,13 +26,29 @@ BinarySearchTree.prototype.insert = function(value){
   };
   findEmptySpace(this, node);
 };
-BinarySearchTree.prototype.contains = function(){};
+BinarySearchTree.prototype.contains = function(value){
+
+  var findValue = function(currentNode){
+    if(currentNode.value === undefined){
+      return false;
+    } else if (currentNode.value === value){
+      return true;
+    } else if(currentNode.value < value){
+      return findValue(currentNode.right);
+    } else if(currentNode.value > value){
+      return findValue(currentNode.left)
+    } else{
+      return false;
+    }
+  };
+  return findValue(this);
+};
 BinarySearchTree.prototype.depthFirstLog = function(){};
 
 var Node = function(value){
   this.value = value;
-  this.right = null;
-  this.left = null;
+  this.right = undefined;
+  this.left = undefined;
 }
 /*
  * Complexity: What is the time complexity of the above functions?
