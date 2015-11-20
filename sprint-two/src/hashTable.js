3d8c691edf693d9a4b830aaee775ab31dc771b8e
egeniesse <egeniesse@gmail.com>
2015-11-19 21:11:48 -0800

    stopping for the night

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,17 +5,19 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if(this._storage.get(index)=== undefined){
-    this._storage.set(index,v);
-  } else {
-    this._storage.each(function(value, key, library){
-      if (value === undefined){
-        this._storage.set(key, v);
-      }
-    });
-  }
+
+    if(this._storage.get(index) === undefined){
+      this._storage.set(index,{k:v});
+    } else {
+      this._storage.each(function(value, key, library){
+        if (value === undefined){
+          this._storage.set(key, {k:v});
+        }
+      });
+    }
 };
 
 HashTable.prototype.retrieve = function(k) {
