38cf446b23f2e426fc674d74a4c14eb0f3fa2cf3
egeniesse <egeniesse@gmail.com>
2015-11-19 19:15:02 -0800

    Passes all tests for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  
+
+  for(var key in this._storage){
+    if(this._storage.hasOwnProperty(item)){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
