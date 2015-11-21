9adb69da9ff9c10bb089fdf1a2e25b823f651138
egeniesse <egeniesse@gmail.com>
2015-11-21 11:21:42 -0800

    Tests for removeFromParent function and Parent property

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,10 +5,12 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "removeFromParent", and "contains", and properties named "value" and "parent"', function() {
     expect(tree.addChild).to.be.a("function");
     expect(tree.contains).to.be.a("function");
     expect(tree.hasOwnProperty("value")).to.equal(true);
+    expect(tree.removeFromParent).to.be.a("function");
+    expect(tree.hasOwnProperty("parent")).to.equal(true);
   });
 
   it('should add children to the tree', function() {
@@ -39,6 +41,22 @@ describe('tree', function() {
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
+  });
+
+  it('should associate a child with a parent', function(){
+    tree.addChild(5);
+    expect(tree.children[0].parent).to.equal(tree);
+    tree.children[0].addChild(6);
+    expect(tree.children[0].children[0].parent).to.equal(tree.children[0]);
+  });
+
+  it('should remove parent property from child node and remove association from the parent', function(){
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].removeFromParent();
+    expect(tree.contains(5)).to.equal(false);
+    expect(tree.children[0].value).to.equal(6);
+    // expect(tree.children[0].parent).to.equal(tree);
   });
 
 });
